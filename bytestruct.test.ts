bfc947f5bf92b146dead8bf3087e9352ffa18d4e
bree <me@bree.dev>
2023-02-05 05:37:24 -0500

    move readme test to to proper testing file

--- bytestruct.test.ts
+++ bytestruct.test.ts
@@ -133,3 +133,40 @@ Deno.test("writeStructInto nested", () => {
   writeStructInto(view, Vertex, data, 0);
   assertEquals(readStructFrom(view, Vertex, 0), data);
 });
+
+Deno.test("readme bytestruct", () => {
+  const Vec3f = bytes`be f64*3`;
+  const Position = bytes`be x:f64 y:f64 z:f64`;
+  const Triangle = bytes`be vertices:${Position}*3`;
+
+  const data = new Uint8Array(sizeOf(Triangle));
+  const view = new DataView(data.buffer);
+
+  assertEquals(
+    writeBytesInto(view, Vec3f, [1, 2, 3], 0),
+    24,
+  );
+
+  assertEquals(
+    readBytesFrom(view, Vec3f, 0),
+    [1, 2, 3],
+  );
+
+  const triangle = {
+    vertices: [
+      { x: 1, y: 2, z: 3 },
+      { x: 4, y: 5, z: 6 },
+      { x: 7, y: 8, z: 9 },
+    ],
+  };
+
+  assertEquals(
+    writeStructInto(view, Triangle, triangle, 0),
+    72,
+  );
+
+  assertEquals(
+    readStructFrom(view, Triangle, 0),
+    triangle,
+  );
+});
